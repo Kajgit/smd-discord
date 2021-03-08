c1f25e5665692772bbed483d373df1de16db2864
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-03-09 00:33:00 +0100

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -2,9 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: You don't have permission to do this.")
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Je hebt hier geen permissies voor.")
 
-    if (!message.channel.name.endsWith(`ticket`)) return message.channel.send(`Please use this command in a ticket.`);
+    if (!message.channel.name.endsWith(`ticket`)) return message.channel.send(`Gelieve deze commando in een ticket te gebruiken.`);
 
     else message.channel.delete();
 
