cd5d3e049905ca7e9f0b1d51f0b4cf3d8e61b077
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-03-09 00:51:32 +0100

    Update suggestpoll.js

--- commands/suggestpoll.js
+++ commands/suggestpoll.js
@@ -22,8 +22,25 @@ module.exports.run = async (bot, message, args) => {
 
     // Verzend het bericht en voeg er reacties aan toe.
     ideeChannel.send(ideeEmbed).then(embedMessage => {
-        embedMessage.react('👍');
-        embedMessage.react('👎');
+        message.react('👍').then(() => message.react('👎'));
+
+        const filter = (reaction, user) => {
+            return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+        };
+
+        message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+			message.reply('you reacted with a thumbs up.');
+		} else {
+			message.reply('you reacted with a thumbs down.');
+		}
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
     });
 
     
