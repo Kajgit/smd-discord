d8430e573244ec8b998ab4e409489e21d353c0a8
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-03-08 23:50:14 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
         let embed = new discord.MessageEmbed()
         .setTitle('Report')
         .setColor('#1c1c1c')
-        .setImage("http://www.cityofhinesville.org/ImageRepository/Document?documentID=2995")
+        .setAvatar("http://www.cityofhinesville.org/ImageRepository/Document?documentID=2995")
         .addField(`Report van:`, message.author.username)
         .addField(`Bug of probleem:`, msg)
         .setFooter(`© SmD 2020`)
