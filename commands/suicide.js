380cfccfa3a50dea131a07a4c434c9b2b337c12c
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-03-10 11:12:15 +0100

    Update suicide.js

--- commands/suicide.js
+++ commands/suicide.js
@@ -2,9 +2,14 @@ const discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     
+        var videos = ['https://thumbs.gfycat.com/LeanGranularCur-max-1mb.gif', ''];
+        var randomArgs = Math.floor(Math.random() * 6) + 1;
+
+        console.log(randomArgs);
+
         let embed = new discord.MessageEmbed()
         .setColor('#1c1c1c')
-        .setImage('https://thumbs.gfycat.com/LeanGranularCur-max-1mb.gif')
+        .setImage()
         .addField(message.author.username, `deed zojuist de zepreh!`)
         .setFooter(`© SmD 2020`)
         .setTimestamp()
