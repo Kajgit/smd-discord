1b8e52c9e48118ff5cacabf344d1eca6de93c7f0
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-03-10 11:16:53 +0100

    Update suicide.js

--- commands/suicide.js
+++ commands/suicide.js
@@ -2,19 +2,31 @@ const discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     
-        var videos = ['https://thumbs.gfycat.com/LeanGranularCur-max-1mb.gif', ''];
+        var videos = ['https://thumbs.gfycat.com/LeanGranularCur-max-1mb.gif', 'https://media.tenor.com/images/40f5c0e1efe84dc48facff2e437371ce/tenor.gif', 'https://thumbs.gfycat.com/ApprehensiveIlliterateCollie-max-1mb.gif',
+                    'https://data.whicdn.com/images/168089324/original.gif'];
         var randomArgs = Math.floor(Math.random() * 6) + 1;
 
-        console.log(randomArgs);
+        if (randomArgs == 4) {
+            let savedEmbed = new discord.MessageEmbed()
+            .setColor('#1c1c1c')
+            .setImage(video[4])
+            .addField(message.author.username, `got saved from doing the zepreh!`)
+            .setFooter(`© SmD 2020`)
+            .setTimestamp()
 
-        let embed = new discord.MessageEmbed()
-        .setColor('#1c1c1c')
-        .setImage()
-        .addField(message.author.username, `deed zojuist de zepreh!`)
-        .setFooter(`© SmD 2020`)
-        .setTimestamp()
+            message.channel.send(savedEmbed)
+        } else {
+            let embed = new discord.MessageEmbed()
+            .setColor('#1c1c1c')
+            .setImage(video[randomArgs])
+            .addField(message.author.username, `deed zojuist de zepreh!`)
+            .setFooter(`© SmD 2020`)
+            .setTimestamp()
 
-        message.channel.send(embed)
+            message.channel.send(embed)
+        }
+
+        
 
 }
  
